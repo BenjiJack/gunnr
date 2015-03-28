f84cc8f9a3adbb1ca925fc96fb66fa9f5de406f8
Benjamin Jack <benjaminjack@gmail.com>
2015-03-28 13:42:43 -0400

    Submitter UserID populated automatically with 8 digit hex value

--- app/models/submitter.rb
+++ app/models/submitter.rb
@@ -8,5 +8,21 @@ class Submitter < ActiveRecord::Base
   validates_confirmation_of    :password, :on=>:create
   validates_length_of    :password, :within => Devise.password_length, :allow_blank => true
 
-  
+  validates_presence_of :userid, :on=>:create
+  validates_format_of :userid, :with => /\A([0-9a-f]{8})\Z/i, :on => :create #matches 8 digit hex
+
+  def self.new_userid
+
+  	@id = SecureRandom.hex(4)
+
+  	#check for duplicate
+  	while Submitter.find_by_userid(@id) != nil
+  		@id = SecureRandom.hex(4)
+  	end
+
+  	return @id
+
+  end
+
+
 end
